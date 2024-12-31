8b78585aeb40b1cd8af6698de24eb74d9283d95b
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-12-31 17:28:48 +0700

    design admin navbar

--- challenge-p2/src/components/AdminNavbar.jsx
+++ challenge-p2/src/components/AdminNavbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from '../assets/h_1.png'
+import { useNavigate } from 'react-router-dom'
 
 function AdminNavbar() {
+    const navigate = useNavigate()
+    const goToHome = () => {
+        navigate('/')
+    }
     return (
-        <div>AdminNavbar</div>
+        <div className='bg-[#CCCCCC] flex justify-center pt-5'>
+            <img className='w-[10%] cursor-pointer' src={logo} alt='logo' onClick={goToHome}/>
+        </div>
     )
 }
 
