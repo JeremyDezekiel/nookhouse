fa5b331c72e716397d45e4d71e2c647364f9c070
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-14 16:47:01 +0700

    handle create user to Firestore when log in with google

--- iproject/client/src/hooks/useLoginGoogle.jsx
+++ iproject/client/src/hooks/useLoginGoogle.jsx
@@ -1,9 +1,25 @@
 import { signInWithPopup } from "firebase/auth"
-import { auth, provider } from "../config/firebase"
+import { auth, db, provider } from "../config/firebase"
+import { doc, setDoc } from "firebase/firestore"
 
 export const useLoginGoogle = async () => {
     try {
-        await signInWithPopup(auth, provider)
+        const userCredential = await signInWithPopup(auth, provider)
+        // create user to firestore
+        if (userCredential && userCredential.user) {
+            const { user } = userCredential
+            const displayName = user.displayName || 'Anonymous'
+            const fullName = user.fullName || ''
+
+            await setDoc(doc(db, 'users', user.uid), {
+                username: displayName,
+                email: user.email,
+                role: 'customer',
+                fullName: fullName,
+                birthday: ''
+            })
+        }
+        return userCredential.user
     } catch (error) {
         console.error(error)
         let customMessage
@@ -11,6 +27,12 @@ export const useLoginGoogle = async () => {
         switch (error.code) {
             case 'auth/user-disabled':
                 customMessage = 'This account has been disabled'
+                break
+            case 'auth/popup-closed-by-user':
+                customMessage = 'Popup closed by the user'
+                break
+            case 'auth/cancelled-popup-request':
+                customMessage = 'Popup cancelled by the user'
                 break
             default:
                 customMessage = 'An error occured during Login'
