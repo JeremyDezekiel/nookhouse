89d42d0bca759e6d24c83af86f2f5b572d13e7f5
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-15 20:10:51 +0700

    first layout product card

---
+++ iproject/client/src/components/ProductUserCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ProductUserCard() {
+    return (
+        <div className='max-h-96'>
+            <img className='mx-auto' src='https://cdn.ruparupa.io/fit-in/240x240/filters:format(webp)/filters:quality(90)/ruparupa-com/image/upload/Products/10124356_2.jpg' alt='' />
+            <p className='mb-5'>Krisbow 1 Ltr Vacuum Cleaner Dry Hand & Upright 400 Watt</p>
+            <div className='flex justify-between'>
+                <span>Rp 500.000</span>
+                <span>100 Terjual</span>
+            </div>
+        </div>
+    )
+}
+
+export default ProductUserCard
--- iproject/client/src/pages/HomePage.jsx
+++ iproject/client/src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProductUserCard from '../components/ProductUserCard'
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div>
+            <div className='w-full text-center text-9xl'>Hero</div>
+            <div>
+                
+            </div>
+            <div>
+                <h1 className='text-center'>Products</h1>
+                <div className='grid grid-cols-6'>
+                    <ProductUserCard/>
+                </div>
+            </div>
+        </div>
     )
 }
 
