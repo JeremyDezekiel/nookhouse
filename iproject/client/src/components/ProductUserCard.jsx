0ca813ea21c0f55435b3b06f0018d91a28da8d51
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-01-24 00:32:57 +0700

    handle if image just 1 image

--- iproject/client/src/components/ProductUserCard.jsx
+++ iproject/client/src/components/ProductUserCard.jsx
@@ -5,16 +5,26 @@ function ProductUserCard({ filteredProduct }) {
     const navigate = useNavigate()
 
     return (
-        <div className='max-h-[450px] cursor-pointer h-full group grid' onClick={() => navigate('/product/' + filteredProduct.id)}>
-            <img className='mx-auto rounded-t-2xl group-hover:hidden' src={filteredProduct.images[0]} alt={filteredProduct.name} />
-            <img className='mx-auto rounded-t-2xl hidden group-hover:block' src={filteredProduct.images[1]} alt={filteredProduct.name} />
-            <p className='mb-5'>{filteredProduct.name}</p>
-            <div className='grid items-end'>
+        <div className='max-h-[450px] cursor-pointer min-h-[450px] group flex flex-col relative' onClick={() => navigate('/product/' + filteredProduct.id)}>
+            {
+                filteredProduct.images.length > 1 ? (
+                    <div className='relative h-[280px]'>
+                        <img className='mx-auto rounded-t-2xl opacity-100 group-hover:opacity-0 transition-opacity duration-500 ease-in-out absolute inset-0' src={filteredProduct.images[0]} alt={filteredProduct.name} />
+                        <img className='mx-auto rounded-t-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out absolute inset-0' src={filteredProduct.images[1]} alt={filteredProduct.name} />
+                    </div>
+                ) : (
+                        <div className='relative h-[280px]'>
+                            <img className='mx-auto rounded-t-2xl opacity-100 group-hover:opacity-100 transition-opacity duration-300 ease-in-out absolute inset-0' src={filteredProduct.images[0]} alt={filteredProduct.name} />
+                        </div>
+                )
+            }
+            <p className='mb-5 mt-5'>{filteredProduct.name}</p>
+            <div className='flex flex-col items-start'>
                 <div className='flex gap-2 items-center'>
                     <p className={`${filteredProduct.discount !== 0 ? 'text-lg line-through' : 'text-xl'}`}>Rp {filteredProduct.price.toLocaleString()}</p>
-                    {filteredProduct.discount !== 0 && <p className='bg-red-500 px-2 rounded-full'>{filteredProduct.discount}%</p>}
-                </div>    
-                { filteredProduct.discount !== 0 && (
+                    {filteredProduct.discount !== 0 && <p className='bg-red-500 px-2 rounded-full text-white mb-2'>{filteredProduct.discount}%</p>}
+                </div>
+                {filteredProduct.discount !== 0 && (
                     <p className='text-xl text-red-500 ms-5'>Rp {filteredProduct.discountPrice.toLocaleString()}</p>
                 )}
             </div>
